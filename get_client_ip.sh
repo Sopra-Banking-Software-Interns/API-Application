2ba10bf0cfb64ac3c1afb765993b37dec6e84305
Prakhar <prakhar.tripathi@ext.soprabanking.com>
2023-06-13 17:01:11 +0530

    Added get_client_country.sh file, client_country.txt file and iso_3166-2data.txt file

--- get_client_ip.sh
+++
@@ -1,4 +0,0 @@
-#execute this script to update the client_ip in client_ip.txt
-client_data=$(curl -s https://api.bigdatacloud.net/data/client-ip)
-client_ip=$(echo "$client_data" | jq -r .ipString)
-echo "$client_ip">>client_ip.txt
